df4d8be8dea2fa47f91662ee45f8037a1b934cd7
Stephen Gruzin <stephengrzn@gmail.com>
2024-05-17 15:23:23 -0700

    Added scripts and pm2config

--- src/lib/sendForm.ts
+++ src/lib/sendForm.ts
@@ -1,14 +1,12 @@
 import { Resend } from "resend";
 
 export async function sendForm(email: string, name: string, desc: string, whereFrom: string | null) {
-    console.log({email, name, desc, whereFrom});
-    return;
     const resend = new Resend(import.meta.env.VITE_RESEND_KEY);
     const { data, error } = await resend.emails.send({
         from: "contact@mail.thelocalfenceco.com",
         to: import.meta.env.VITE_EMAIL,
         subject: 'Website Contact Form',
-        html:  `<h1>From: ${email}</h1><h2>Name: ${name}</h2><p>Description: ${desc}</p>`
+        html:  `<h1>From: ${email}</h1><h2>Name: ${name}</h2><p>Description: ${desc}</p><p>From: ${whereFrom ? whereFrom : "unknown"}</p>`
     });
 
     if (error) {
