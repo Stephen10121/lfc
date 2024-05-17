91901168ba3bd63cdd08fb7a69612ed075e26b2f
Stephen Gruzin <stephengrzn@gmail.com>
2024-05-16 23:30:44 -0700

    Added a loading animation.

---
+++ src/lib/Loading.svelte
@@ -0,0 +1,29 @@
+<div></div>
+
+
+<style>
+    div {
+        position: fixed;
+        z-index: 200;
+        top: 0;
+        left: 0;
+        width: 100%;
+        height: 4px;
+        background: rgba(215,157,64,1);
+        background-image: linear-gradient(90deg, rgba(215,157,64,1) 0%, rgba(252,252,252,1) 35%, rgba(215,157,64,1) 100%);
+        background-size: 400% 400%;
+	    animation: gradient 5s ease infinite;
+    }
+
+    @keyframes gradient {
+        0% {
+            background-position: 0% 50%;
+        }
+        50% {
+            background-position: 100% 50%;
+        }
+        100% {
+            background-position: 0% 50%;
+        }
+    }
+</style>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,9 +1,15 @@
 <script lang="ts">
+    import { navigating } from "$app/stores";
     import Footer from "$lib/Footer.svelte";
     import Header from "$lib/Header.svelte";
+    import Loading from "$lib/Loading.svelte";
 </script>
 
 <Header />
+
+{#if $navigating}
+    <Loading />
+{/if}
 
 <slot />
 
