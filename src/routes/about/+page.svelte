4ccc8f660ef6f0bf91c998f6b5010c213a1fb318
Stephen Gruzin <stephengrzn@gmail.com>
2024-08-16 18:50:57 -0700

    Changing the title to match serp title

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -10,21 +10,21 @@
 
 <svelte:head>
     <link rel="canonical" href="https://thelocalfenceco.com/about" />
-    <title>About Us | Local Fence Co</title>
-    <meta name="title" content="About Us | Local Fence Co" />
+    <title>About Us - Local Fence Co</title>
+    <meta name="title" content="About Us - Local Fence Co" />
 	<meta name="description" content="Fence installation and repair business servicing the Clark County area. It's our dedication to provide a quality, affordable service, and 100% satisfaction." />
 
     <!-- Open Graph / Facebook -->
     <meta property="og:type" content="website" />
     <meta property="og:url" content="https://thelocalfenceco.com/about" />
-    <meta property="og:title" content="About Us | Local Fence Co" />
+    <meta property="og:title" content="About Us - Local Fence Co" />
     <meta property="og:description" content="Fence installation and repair business servicing the Clark County area. It's our dedication to provide a quality, affordable service, and 100% satisfaction." />
     <meta property="og:image" content="https://thelocalfenceco.com/logo.png" />
 
     <!-- Twitter -->
     <meta property="twitter:card" content="summary_large_image" />
     <meta property="twitter:url" content="https://thelocalfenceco.com/about" />
-    <meta property="twitter:title" content="About Us | Local Fence Co" />
+    <meta property="twitter:title" content="About Us - Local Fence Co" />
     <meta property="twitter:description" content="Fence installation and repair business servicing the Clark County area. It's our dedication to provide a quality, affordable service, and 100% satisfaction." />
     <meta property="twitter:image" content="https://thelocalfenceco.com/logo.png" />
 </svelte:head>
