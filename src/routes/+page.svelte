fc18b91143112d1b507d84a7488dd7cd94a34c9d
Stephen Gruzin <stephengrzn@gmail.com>
2024-08-16 18:50:03 -0700

    I used the wrong title

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -11,21 +11,21 @@
 
 <svelte:head>
     <link rel="canonical" href="https://thelocalfenceco.com/" />
-    <title>Home | Local Fence Co</title>
-    <meta name="title" content="Home | Local Fence Co" />
+    <title>Local Fence Co: Home</title>
+    <meta name="title" content="Local Fence Co: Home" />
     <meta name="description" content="Local Fence Co is your premier fence contractor in Vancouver, WA, dedicated to transforming your property into a safe and stylish sanctuary." />
 
     <!-- Open Graph / Facebook -->
     <meta property="og:type" content="website" />
     <meta property="og:url" content="https://thelocalfenceco.com/" />
-    <meta property="og:title" content="Home | Local Fence Co" />
+    <meta property="og:title" content="Local Fence Co: Home" />
     <meta property="og:description" content="Local Fence Co is your premier fence contractor in Vancouver, WA, dedicated to transforming your property into a safe and stylish sanctuary." />
     <meta property="og:image" content="https://thelocalfenceco.com/logo.png" />
 
     <!-- Twitter -->
     <meta property="twitter:card" content="summary_large_image" />
     <meta property="twitter:url" content="https://thelocalfenceco.com/" />
-    <meta property="twitter:title" content="Home | Local Fence Co" />
+    <meta property="twitter:title" content="Local Fence Co: Home" />
     <meta property="twitter:description" content="Local Fence Co is your premier fence contractor in Vancouver, WA, dedicated to transforming your property into a safe and stylish sanctuary." />
     <meta property="twitter:image" content="https://thelocalfenceco.com/logo.png" />
 </svelte:head>
