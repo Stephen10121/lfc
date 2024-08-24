456d96ac07cd634ffbbf0f930d5b71fab81c6f7a
Stephen Gruzin <stephengrzn@gmail.com>
2024-08-23 18:02:50 -0700

    Added more reviews

--- src/lib/reviews.ts
+++ src/lib/reviews.ts
@@ -11,5 +11,13 @@ export const reviews: Review[] = [
     {
         name: "D N S",
         message: "This guy knows what he's doing! Did a great job installing driveway gates. Cleaned up after the project was complete. Honest, hard working, and great rates. I'll definitely have him back for installing metal iron fence."
+    },
+    {
+        name: "BYU",
+        message: "Very satisfied customer! Bogdan is an ultimate professional who did exactly as we agreed and was affordable. He fixed/reinstalled a double vinyl fence gate most fence companies could not. Bogdan is by far the most knowledgeable fence installer I have ever met, and believe me - I have met a lot of them. I highly recommend him because of his honesty, professionalism, knowledge and much more!"
+    },
+    {
+        name: "Joe Balmores",
+        message: "Bogdon did a magnificent job on our backyard fence. Very articulate of his measurements and making sure that all the 4x4’s, slats and the flat top was leveled. I would recommend him to all friends and neighbors to build future fencing"
     }
 ]
